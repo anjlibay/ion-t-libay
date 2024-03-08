e310c053fff601463d51916c220d11970de4dc6b
anjlibay <libay.angela01@gmail.com>
2024-03-08 08:35:11 +0800

    added vcard to the profile

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,  IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -7,15 +7,27 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Profile</IonTitle>
           </IonToolbar>
         </IonHeader>
+
+        <IonCard>
+              <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+              <IonCardHeader>
+                <IonCardTitle>Card Title</IonCardTitle>
+                <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+              </IonCardHeader>
+
+              <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+        </IonCard>
+
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
